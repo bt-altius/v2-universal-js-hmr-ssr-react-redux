486a8ad3c6f3cbeca493da483949cac9df0f8e04
Benjamin Tudor <btudor@altius.org>
2018-08-24 17:24:24 -0700

    (add): upgrade step 1 - new versions of babel-loader, react-hot-loader, webpack, webpack-dev-middleware, webpack-hot-middleware

--- src/server/server.js
+++ src/server/server.js
@@ -48,5 +48,5 @@ const server = http.createServer(app);
 
 server.listen(8080, function() {
    const address = server.address();
-   console.log(`${'>>>'.cyan} ${'Listening on:'.rainbow} ${'localhost::'.trap.magenta}${`${address.port}`.green}`);
+   console.log(`[${process.env.NODE_ENV.cyan}] Listening on http://localhost:${address.port}`);
  });
